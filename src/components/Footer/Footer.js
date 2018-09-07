10f9fe78aa0362c7d2fce04a7ead0dba5502ab9c
Austin.Wiedenman <awiedenman@gmail.com>
2018-09-07 17:02:00 -0600

    Build out footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './Footer.css';
+
+export const Footer = (props) => {
+  return (
+    <footer className="footer_container">
+    FOOTER
+      {/* <NavLink>user info</NavLink>
+      <NavLink>home</NavLink>
+      <NavLink>tickets</NavLink> */}
+    </footer>
+  )
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App/App';
+import App from './components/App/App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers/';
-import { BrowserRouter } from 'react-router-dom'
+import rootReducer from './reducers';
+import { BrowserRouter } from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
@@ -17,3 +18,4 @@ ReactDOM.render(
     <App />
   </BrowserRouter>
 </Provider>, document.getElementById('root'));
+registerServiceWorker();
