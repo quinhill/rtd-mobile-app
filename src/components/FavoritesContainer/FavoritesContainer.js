cfd3d175acc24ef83e12c30c7d933d9db8c363d7
Austin.Wiedenman <awiedenman@gmail.com>
2018-09-09 14:40:20 -0600

    Render Favorite card from mockData on page closes #29, closes #37

--- src/components/FavoritesContainer/FavoritesContainer.js
+++ src/components/FavoritesContainer/FavoritesContainer.js
@@ -5,9 +5,17 @@ import { connect } from 'react-redux';
 export class FavoritesContainer extends Component {
   
   render(){
-   
+    const { favorites } = this.props;
+    const createFavCards = favorites.map((favoriteRoute, index) => {
+      return (<Favorite
+        name={favoriteRoute.name}
+        key={index}
+      />);
+    });
+
     return (
       <div>
+        {createFavCards}
       </div>
     );
   }
