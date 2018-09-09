e7d34642f81fbe60df38e891a638750ebb3f13c4
Austin.Wiedenman <awiedenman@gmail.com>
2018-09-09 16:58:52 -0600

    CSS styling for favorites

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -21,13 +21,14 @@ export class Favorite extends Component {
         className='favorite_card'
         onClick={this.showDetails}
       >
-        <div>
+        <div className='favorite_card-name'>
           <h2>
             {this.props.name}
           </h2>
         </div>
         <div className='favorite_trip-details' hidden={this.state.hidden}>
           <h4 className='starting-point'>{this.props.startingPoint}</h4>
+          <p>←→</p>
           <h4 className='destination'>{this.props.destination}</h4>
         </div>
       </div>
