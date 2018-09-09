f19ea6208b01c39035be1bdb844510c2e9240468
Quinlan Hill <quinhill@icloud.com>
2018-09-09 15:21:41 -0600

    style navlinks in footer. closes #28

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { displaySelected } from '../../actions';
-import PropTypes from 'prop-types';
 
 import './Footer.css';
 
@@ -11,52 +8,31 @@ export const Footer = (props) => {
   const selectDisplay = (event) => {
     event.preventDefault();
     const { id } = event.target;
-    console.log(id);
     props.selectDisplay(id);
   };
 
   return (
     <footer className="footer_container">
-      {/* <NavLink>user info</NavLink> */}
       <NavLink 
         to='/favorites'
+        id="favorites"
+        class="icon"
       >
-        <img 
-          src="/images/user.svg" 
-          className="icon"
-          onClick={selectDisplay}
-          id="favorites"
-        />
       </NavLink>
       <NavLink 
-        to='/'
+        exact to='/'
+        id="home"
+        class="icon"
       >
-        <img 
-          src="/images/home.svg" 
-          className="icon" 
-          onClick={selectDisplay}
-          id="home"
-        />
       </NavLink>
       <NavLink 
         to='/tickets'
+        id="tickets"
+        class="icon"
       >
-        <img 
-          src="/images/tickets.svg" className="icon" 
-          onClick={selectDisplay}
-          id="tickets"
-        />
       </NavLink> 
     </footer>
   );
 };
 
-export const mapDispatchToProps = dispatch => ({
-  selectDisplay: (selected) => dispatch(displaySelected(selected))
-});
-
-export default connect(null, mapDispatchToProps)(Footer);
-
-Footer.propTypes = {
-  selectDisplay: PropTypes.func
-};
+export default Footer;
--- src/reducers/displayReducer.js
+++ src/reducers/displayReducer.js
@@ -2,7 +2,7 @@ const display = (state = 'home', action) => {
   switch (action.type) {
   case 'DISPLAY_SELECTED':
     return action.selected;
-  default:
+  default: 
     return state;
   }
 };
