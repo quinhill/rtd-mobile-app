79c408db255866a916139ca8543399e7ef135987
Quinlan Hill <quinhill@icloud.com>
2018-09-11 09:02:39 -0600

    Continue work on firebase

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Home from '../Home/Home';
 import Footer from '../Footer/Footer';
 import TicketsContainer from '../TicketsContainer/TicketsContainer';
 import Search from '../../containers/Search/Search';
 import FavoritesContainer from '../FavoritesContainer/FavoritesContainer';
 import Header from '../Header/Header';
+import Navigation from '../Navigation/Navigation';
 
 import './App.css';
 
@@ -15,10 +16,7 @@ export class App extends Component {
       <div className="App">
         <div className="overlay">
           <Header />
-          <Route exact path='/' component={ Home }/>
-          <Route exact path='/favorites' component={ FavoritesContainer }/>
-          <Route exact path='/search' component={ Search }/>
-          <Route exact path= '/tickets' component={ TicketsContainer } />     
+          <Navigation />
           <Footer />
         </div>
       </div>
---
+++ src/constants/routes.js
@@ -0,0 +1,8 @@
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const LANDING = '/landing';
+export const HOME = '/';
+export const FAVORITES = '/favorites';
+export const SEARCH = '/search';
+export const TICKETS = '/tickets';
+export const PASSWORD_FORGET = '/pw-forget';
