6ca45d13b630b757a1a61a533d737c84f56f807e
quinhill@icloud.com <quinhill@icloud.com>
2018-09-14 08:06:57 -0600

    Add dropdown for arrival/departure time

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -32,32 +32,57 @@ export class Search extends Component {
   render(){
     return (
       <div className="search_container">
-      <StartPointInput />
-      <EndPointInput />
-        {/* <form onSubmit={ this.handleSubmit }>
-          <input
-            id="starting_location"
-            className="search-input"
-            onChange={ this.handleChange }
-            type="text" 
-            name="startPoint"
-            value={ this.state.startPoint } 
-            placeholder="Starting Location" 
-          />
+        <StartPointInput />
+        <EndPointInput />
+        <form className='time'>
+          <select name='hours'>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
+            <option value='6'>6</option>
+            <option value='7'>7</option>
+            <option value='8'>8</option>
+            <option value='9'>9</option>
+            <option value='10'>10</option>
+            <option value='11'>11</option>
+            <option value='12'>12</option>
+          </select>
+          <select name='minutes'>
+            <option value='00'>00</option>
+            <option value='05'>05</option>
+            <option value='10'>10</option>
+            <option value='15'>15</option>
+            <option value='20'>20</option>
+            <option value='25'>25</option>
+            <option value='30'>30</option>
+            <option value='35'>35</option>
+            <option value='40'>40</option>
+            <option value='45'>45</option>
+            <option value='50'>50</option>
+            <option value='55'>55</option>
+          </select>
           <input 
-            id="destination"
-            className="search-input"
-            onChange={ this.handleChange }
-            type="text" 
-            name='destination' 
-            value={ this.state.destination } 
-            placeholder="Destination" 
+            id='departure'
+            className='departure-arrival' 
+            type='radio' 
+            value='departing'
           />
-          <button 
-            className="search_button"
-            type='submit'>Search
-          </button>
-        </form>    */}
+          <label HTMLfor='departure'>
+            departing
+          </label>
+          <input
+            id='arrival'
+            className='departure-arrival' 
+            type='radio' 
+            value='arriving'
+          />
+          <label HTMLfor='arrival'>
+            arriving
+          </label>
+          <p>at</p>
+        </form>
       </div>
     );
   }
