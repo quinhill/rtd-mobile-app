0e58849aac1fc9184e15048414d0659d94460cd7
quinhill@icloud.com <quinhill@icloud.com>
2018-09-18 05:42:04 -0600

    Refactor accountPage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,45 +1,14 @@
 import React from 'react';
 import './Header.css';
-import SignOutButton from '../../containers/SignOut/SignOut';
-import { Link } from 'react-router-dom';
-
-import AuthUserContext from '../AuthUserContext';
-import * as routes from '../../constants/routes';
-
   
 const Header = () => {
-  const HeaderNonAuth = () => {
-    return (
-      <header>
-        <img 
-          src="https://i.imgur.com/BmUB9ud.png" 
-          className="logo-banner" 
-        />
-        <Link 
-          to={routes.SIGN_IN}
-          className='sign-in-link'
-        >
-          Sign In
-        </Link>
-      </header>
-    );
-  };
-  
-  const HeaderAuth  = () => {
-    return (
-      <header>
-        <img src="/rtd-logo.png" className="logo-banner" />
-      </header>
-    ); 
-  };
-  
   return (
-    <AuthUserContext.Consumer>
-      { authUser => authUser
-        ? <HeaderAuth />
-        : <HeaderNonAuth />
-      }
-    </AuthUserContext.Consumer>
+    <header>
+      <img 
+        src="https://i.imgur.com/BmUB9ud.png" 
+        className="logo-banner" 
+      />
+    </header>
   );
 };
 
