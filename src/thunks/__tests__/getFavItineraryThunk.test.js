e45d60724b4bcdf676f8104ecd5aa6ab333114e5
Austin.Wiedenman <awiedenman@gmail.com>
2018-09-19 10:09:34 -0600

    Testing for getFavItineraryThunk

---
+++ src/thunks/__tests__/getFavItineraryThunk.test.js
@@ -0,0 +1,48 @@
+import { getFavItineraryThunk } from '../getFavItineraryThunk';
+import { hasErrored, isLoading, storeItinerary } from "../../actions";
+
+
+describe('getFavItineraryThunk', () => {
+  let mockUrl;
+  let mockDispatch;
+
+  beforeEach(() => {
+    mockUrl = 'www.mockUrl.com';
+    mockDispatch = jest.fn();
+  })
+
+  it("calls dispatch with the isLoading action ", () => {
+    const thunk = getFavItineraryThunk(mockUrl);
+
+    thunk(mockDispatch);
+
+    expect(mockDispatch).toHaveBeenCalledWith(isLoading(true));
+  });
+
+
+  it('should dispatch hasErrored(true) if the response is not ok', async () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      ok: false
+    }));
+
+    const thunk = getFavItineraryThunk(mockUrl);
+
+    await thunk(mockDispatch);
+
+    expect(mockDispatch).toHaveBeenCalledWith(({ "isLoading": true, "type": "IS_LOADING" }));
+    expect(mockDispatch).not.toHaveBeenCalledWith(isLoading(false));
+  });
+
+  it('should dispatch isLoading(false) if the response is ok', async () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      ok: true
+    }));
+
+    const thunk = getFavItineraryThunk(mockUrl);
+
+    await thunk(mockDispatch);
+
+    expect(mockDispatch).toHaveBeenCalledWith(isLoading(false));
+  });
+
+});
