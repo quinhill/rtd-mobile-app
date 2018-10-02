51e27592b7d7163bac2972c58351cf2d6e8c4d8b
quinhill@icloud.com <quinhill@icloud.com>
2018-10-02 08:00:33 -0600

    Add toggle button for favorites and recent searches

--- src/containers/RecentSearch/RecentSearch.js
+++ src/containers/RecentSearch/RecentSearch.js
@@ -2,21 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 
-
 class RecentSearch extends Component {
+
   render() {
     return (
       <div></div>
-    )
+    );
   }
 }
 
 export const mapStateToProps = state => ({
-  
+  user: state.user,
+  recentSearches: state.recentSearches
 });
-
-export const mapDispatchToProps = dispatch => ({
-  getRecent: url => dispatch(getRecentThunk(url))
-})
 
 export default connect(mapStateToProps, mapDispatchToProps)(RecentSearch);
